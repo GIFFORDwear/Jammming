16b3ca80d00dc536fec2ae4dcd079de2bc489e70
GIFFORDwear <hedronnft@gmail.com>
2023-02-23 14:32:19 -0700

    Started by adding all base level React Components

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+{/* import 'SearchBar', 'SearchResults', and 'Playlist' */}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+            {/* SearchBar */}
+          <div className="App-playlist">
+            {/* SearchResults component */}
+            {/* Playlist component */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
